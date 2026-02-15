3855ba2e60ddf71d654567fac617c7d7a8e0456c
sanaullah129 <asanaullah129@gmail.com>
2026-02-15 17:14:38 +0000

    login page -- wip

--- client/src/modules/auth/Login.tsx
+++ client/src/modules/auth/Login.tsx
@@ -1,50 +1,12 @@
-import {
-  Box,
-  TextField,
-  Button,
-  Typography,
-  Alert,
-  CircularProgress,
-} from "@mui/material";
+import { Box, TextField } from "@mui/material";
 
 const Login = () => {
   return (
-    <Box
-      component="form"
-      onSubmit={() => {}}
-      sx={{
-        maxWidth: 420,
-        mx: "auto",
-        mt: 8,
-        display: "flex",
-        flexDirection: "column",
-        gap: 2,
-      }}
-    >
-      <Typography variant="h5">Sign in</Typography>
-      <TextField
-        label="Email"
-        type="email"
-        value={""}
-        onChange={() => {}}
-        required
-      />
-      <TextField
-        label="Password"
-        type="password"
-        value={""}
-        onChange={() => {}}
-        required
-      />
-      <Button
-        type="submit"
-        variant="contained"
-        disabled={false}
-        startIcon={false ? <CircularProgress size={18} /> : undefined}
-      >
-        {false ? "Signing in..." : "Sign in"}
-      </Button>
-    </Box>
+    <>
+      <Box>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+      </Box>
+    </>
   );
 };
 
